a9ab872e8b3fe15956db1ae1e13b4a773babe33d
juangalbea <juangalbea@gmail.com>
2025-04-01 11:17:30 +0100

    start modal

--- canvas.js
+++ canvas.js
@@ -1,3 +1,31 @@
+// const updateButton = document.getElementById("updateDetails");
+const cancelButton = document.getElementById("cancel");
+const dialog = document.getElementById("favDialog");
+dialog.returnValue = "favAnimal";
+
+function openCheck(dialog) {
+  if (dialog.open) {
+    console.log("Dialog open");
+  } else {
+    console.log("Dialog closed");
+  }
+}
+
+// Update button opens a modal dialog
+// updateButton.addEventListener("click", () => {
+//   dialog.showModal();
+//   openCheck(dialog);
+// });
+dialog.showModal();
+openCheck(dialog);
+
+// Form cancel button closes the dialog box
+cancelButton.addEventListener("click", () => {
+  dialog.close("animalNotChosen");
+  openCheck(dialog);
+  init();
+});
+
 var canvas = document.querySelector("canvas");
 
 canvas.width = window.innerWidth;
@@ -191,8 +219,6 @@ function animate() {
     sendCircles[i].update();
   }
 }
-
-init();
 
 animate();
 
